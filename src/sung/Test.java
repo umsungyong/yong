867751ff22dff0642a7f5c631d115c1f66aea6a4
zhxms <zhxms@DESKTOP-1I0VJRR>
2020-06-15 17:50:47 +0900

    adsasd

--- src/sung/Test.java
+++ src/sung/Test.java
@@ -2,7 +2,6 @@ package sung;
 
 public class Test{
     public static void main(String[] args) {
-               ㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ
-            //ㅋㅋ	
+              //ㅋㅋ	
      }
  }
--- src/sung/Yong.java
+++
@@ -1,5 +0,0 @@
-package sung;
-
-public class Yong {
-    실험 
-}
